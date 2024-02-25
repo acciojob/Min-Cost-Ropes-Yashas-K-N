f6941d50eed4b520783cde1ae7d874fb9a1ef571
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2024-02-25 18:59:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
+
+
+
+
+	{ 
+	let sum = 0;
+	let min = 0;
+
+	for (let i =0; i< arr.length; i++){
+		sum+=arr[i];
+		if (sum<min){
+            min = sum;
+		}
+		return min;
   
 }
+mincost([]);
 
 module.exports=mincost;
